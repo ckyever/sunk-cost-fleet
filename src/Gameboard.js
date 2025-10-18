3b1d2b9f8f07bfa6a56a16bfb98d9b8ff276d53b
ckyever <ckyeverg@gmail.com>
2025-10-18 18:44:53 +1100

    Add ability to place a ship by coordinates

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,3 +1,5 @@
+import { Ship } from "./Ship.js";
+
 export class Gameboard {
   constructor(size) {
     this.size = size;
@@ -12,5 +14,49 @@ export class Gameboard {
       board.push(row);
     }
     return board;
+  }
+
+  place(coordinateList) {
+    // Check all coordinates are valid first
+    // We use a standard for loop so we can exit early when finding an invalid coordinate
+    for (let i = 0; i < coordinateList.length; i++) {
+      let rowIndex;
+      let columnIndex;
+      try {
+        rowIndex = coordinateList[i][0];
+        columnIndex = coordinateList[i][1];
+      } catch {
+        throw new Error(`Invalid coordinate - ${coordinateList[i]}`);
+      }
+      if (!this.#isCoordinateIndexValid(rowIndex)) {
+        return false;
+      }
+      if (!this.#isCoordinateIndexValid(columnIndex)) {
+        return false;
+      }
+    }
+
+    // Create ship
+    const newShip = new Ship(coordinateList.length);
+    this.ships.push(newShip);
+
+    // Place the ship on the board
+    coordinateList.forEach((coordinate) => {
+      let rowIndex;
+      let columnIndex;
+      try {
+        rowIndex = coordinate[0];
+        columnIndex = coordinate[1];
+      } catch {
+        throw new Error(`Invalid coordinate - ${coordinate}`);
+      }
+      this.board[rowIndex][columnIndex] = newShip.id;
+    });
+
+    return true;
+  }
+
+  #isCoordinateIndexValid(coordinateIndex) {
+    return coordinateIndex >= 0 && coordinateIndex < this.size;
   }
 }
--- src/test/Gameboard.test.js
+++ src/test/Gameboard.test.js
@@ -32,3 +32,43 @@ test("Create large gameboard", () => {
     [null, null, null, null, null, null, null, null, null, null],
   ]);
 });
+
+test("Place horizontal small ship", () => {
+  const game = new Gameboard(4);
+  game.place([
+    [0, 0],
+    [0, 1],
+  ]);
+  const shipId = game.ships[0].id;
+  expect(game.board).toEqual([
+    [shipId, shipId, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+  ]);
+});
+
+test("Place vertical small ship", () => {
+  const game = new Gameboard(4);
+  game.place([
+    [0, 0],
+    [1, 0],
+  ]);
+  const shipId = game.ships[0].id;
+  expect(game.board).toEqual([
+    [shipId, null, null, null],
+    [shipId, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+  ]);
+});
+
+test("Place small ship outside of board", () => {
+  const game = new Gameboard(4);
+  expect(
+    game.place([
+      [0, 0],
+      [-1, 0],
+    ]),
+  ).toBe(false);
+});
